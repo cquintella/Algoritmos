a0f63155ea7644673dfaed8034efe5702ff5ae72
Carlos Alvaro Quintella <caquintella@gmail.com>
2023-03-13 12:23:23 -0300

    Create linha_linha.c

---
+++ exemplos/linha_linha.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void lerArquivoLinhaPorLinha(const char *nomeArquivo) {
+    FILE *arquivo;
+    char linha[1000];
+
+    arquivo = fopen(nomeArquivo, "r");
+    if (arquivo == NULL) {
+        printf("Erro ao abrir o arquivo %s.\n", nomeArquivo);
+        exit(1);
+    }
+
+    while (fscanf(arquivo, "%[^\n]%*c", linha) != EOF) {
+        printf("%s\n", linha);
+    }
+
+    fclose(arquivo);
+}
