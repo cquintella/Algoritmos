82dddb9e14a9d854c8c100a3d6d44532be2e7e7b
Carlos Alvaro Quintella <caquintella@gmail.com>
2023-03-28 08:00:15 -0300

    Update hashing.c

--- exemplos/hashing.c
+++ exemplos/hashing.c
@@ -1,1 +1,19 @@
+#include <string.h>
+#define HASH_SIZE=100
 
+unsigned int hashing(char *value){
+  unsigned int hash=0;
+  for(i=0;i<strlen[value];i++){
+      hash=(unsigned int) value[i]+hash;
+  }
+  return (hash % mod[HASH_SIZE]);   
+}
+
+int main(){
+  char *value;
+  printf("Digite algum texto: "):
+  scanf(%s,value);
+  prinft("\n simple hash of : %s is %d",value,hashing(value));
+}
+  
+    
